c89fe8e505c76a155a68a951786453c6b6e6f71d
Md Alkama <mdalkamadad@gmail.com>
2026-03-15 16:56:04 +0530

    chore: env configure

--- Backend/src/configs/env.config.js
+++ Backend/src/configs/env.config.js
@@ -5,7 +5,8 @@ dotenv.config()
 const CONFIG = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
-    SERVER_HOST: process.env.SERVER_HOST
+    SERVER_HOST: process.env.SERVER_HOST,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
 }
 
 export default CONFIG
