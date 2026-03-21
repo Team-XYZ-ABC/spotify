98447455bce2d1fdc6d780e088522fc931fa869f
Md Alkama <mdalkamadad@gmail.com>
2026-03-21 22:54:49 +0530

    fix: refactor authentication middleware for improved error handling

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,15 +1,29 @@
-import CONFIG from '../configs/env.config';
+import CONFIG from '../configs/env.config.js'
 import jwt from 'jsonwebtoken'
 
-const auth = async(req,res, next)=>{
-    const token = req.cookies.token;
-    if(!token){
-        return res.status(400).json({
-            message: "You are unauthorized",
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookies?.token
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Unauthorized - Please login!",
+                success: false
+            })
+        }
+
+        const decoded = jwt.verify(token, CONFIG.JWT_SECRET_KEY)
+
+        req.user = decoded.user
+
+        next()
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Unauthorized",
             success: false
         })
     }
-
-    const decoded = jwt.verify(token, CONFIG.JWT_SECRET_KEY)
-    req.user = decoded.user
 }
+
+export default isAuthenticated
