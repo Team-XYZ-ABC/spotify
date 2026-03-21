4e3e57bac0a6ac8ebee67203710b39a9fe2b9604
Shivangi <shiivangii.gupta@gmail.com>
2026-03-22 00:45:20 +0530

    feat: add uploadSingle, uploadimage, uploadMultiple and uploadTrack controllers for uploading the track

--- Backend/src/controllers/track.controller.js
+++ Backend/src/controllers/track.controller.js
@@ -1,3 +1,7 @@
+import { uploadFile } from "../services/storage.services.js";
+
+
+
 export const getTrack = (req, res) => {
   res.send("GET TRACK DETAILS");
 };
@@ -26,8 +30,105 @@ export const getTrackRecommendations = (req, res) => {
   res.send("GET TRACK RECOMMENDATIONS");
 };
 
-export const uploadTrack = (req, res) => {
-  res.send("UPLOAD TRACK");
+export const uploadSingle = async (req, res) => {
+  try {
+    const file = req.file;
+    console.log(file)
+
+    if (!file) {
+      return res.status(400).json({ message: "No file provided" });
+    }
+
+    const result = await uploadFile(
+      file.buffer,
+      file.originalname,
+      "uploads/single"
+    );
+
+    res.json({
+      message: "File uploaded",
+      data: result,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const uploadImage = async (req, res) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "No file" });
+    }
+
+    if (!file.mimetype.startsWith("image")) {
+      return res.status(400).json({ message: "Only image allowed" });
+    }
+
+    const result = await uploadFile(
+      file.buffer,
+      file.originalname,
+      "uploads/images"
+    );
+
+    res.json({ message: "Image uploaded", data: result });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const uploadTrack = async (req, res) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "No file" });
+    }
+
+    if (!file.mimetype.startsWith("audio")) {
+      return res.status(400).json({ message: "Only audio allowed" });
+    }
+
+    const result = await uploadFile(
+      file.buffer,
+      file.originalname,
+      "uploads/tracks"
+    );
+
+    res.json({ message: "Track uploaded", data: result });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const uploadMultiple = async (req, res) => {
+  try {
+    const files = req.files;
+
+    if (!files || files.length === 0) {
+      return res.status(400).json({ message: "No files" });
+    }
+
+    const results = [];
+
+    for (let file of files) {
+      const uploaded = await uploadFile(
+        file.buffer,
+        file.originalname,
+        "uploads/multiple"
+      );
+      results.push(uploaded);
+    }
+
+    res.json({ message: "Files uploaded", data: results });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateTrack = (req, res) => {
