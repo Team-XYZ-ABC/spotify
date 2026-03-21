2a50b02669b0ef509f79d011e90e5638a201613c
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-21 23:21:33 +0530

    feat: add profile retrieval endpoint to authentication routes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
+import { GetCurrentUser, loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
 
 const authRouter = Router()
 
 authRouter.post('/users/register', registerUser)
 authRouter.post('/users/login', loginUser)
 authRouter.post('/users/logout', logoutUser)
+authRouter.get('/users/profile', GetCurrentUser)
 
 export default authRouter
