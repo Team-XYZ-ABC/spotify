07bb13ed65b66cbf49ff2b9e80fdba349f825a9d
Shivangi <shiivangii.gupta@gmail.com>
2026-03-22 00:43:24 +0530

    feat: add multer service for files upload

--- Backend/src/configs/env.config.js
+++ Backend/src/configs/env.config.js
@@ -6,7 +6,8 @@ const CONFIG = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     SERVER_HOST: process.env.SERVER_HOST,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY
 }
 
 export default CONFIG
