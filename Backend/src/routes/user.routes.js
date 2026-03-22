9ee21680ebd5f4c8ed13c9b6e382cc409b4d81b9
Md Alkama <mdalkamadad@gmail.com>
2026-03-22 23:20:43 +0530

    fix: add authentication middleware to the profile retrieval route

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getOtherUserProfile, getProfile, updateProfile, getHistory, getRecentlyPlayed } from "../controllers/users.controller.js";
+import isAuthenticated from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get('/me', getProfile)
+userRouter.get('/me', isAuthenticated, getProfile)
 userRouter.patch('/me', updateProfile)
 userRouter.get('/:id', getOtherUserProfile)
 userRouter.get('/me/history', getHistory)
