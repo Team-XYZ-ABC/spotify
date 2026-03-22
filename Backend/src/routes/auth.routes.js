2bb7ca351fcff9c605faa922a9cb946132dbaec3
Md Alkama <mdalkamadad@gmail.com>
2026-03-23 01:32:12 +0530

    fix: correct casing of getCurrentUser import in auth.routes.js

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { GetCurrentUser, loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
+import { getCurrentUser, loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
 
 const authRouter = Router()
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
 authRouter.post('/logout', logoutUser)
-authRouter.get('/me', GetCurrentUser)
+authRouter.get('/me', getCurrentUser)
 
 export default authRouter
