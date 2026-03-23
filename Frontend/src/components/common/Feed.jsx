aa31f0963bce8c2ad4009e419ae2313c00917974
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-24 01:26:15 +0530

    feat: Sidebar ui done

--- Frontend/src/components/common/Feed.jsx
+++ Frontend/src/components/common/Feed.jsx
@@ -4,12 +4,21 @@ import Footer from "./Footer";
 const Feed = () => {
   return (
     <div className="flex-1 h-[calc(100vh-86px)] no-scrollbar overflow-y-auto bg-zinc-900 rounded-lg flex flex-col">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 p-6 text-white">
-        
-        {Array(20).fill().map((_, i) => (
-          <div key={i} className="h-66 cursor-pointer w-full rounded-md bg-zinc-800 hover:bg-zinc-900 transition"></div>
+      <div className="flex flex-col gap-3 p-6 text-white">
+        {Array(5).fill().map((_, i) => (
+          <div
+            key={i}
+            className="h-70 p-2 cursor-pointer flex flex-col gap-4 w-full rounded-md transition"
+          >
+            <h1 className="text-xl">Genre Category</h1>
+            <div className="flex gap-4 no-scrollbar overflow-x-auto">
+              {Array(8).fill().map((_, i) => (
+              <div className="h-58 w-54 shrink-0 bg-zinc-950 rounded-lg">
+              </div>
+            ))}
+            </div>
+          </div>
         ))}
-
       </div>
       <Footer />
     </div>
--- Frontend/src/components/common/Sidebar.jsx
+++ Frontend/src/components/common/Sidebar.jsx
@@ -4,22 +4,66 @@ const Sidebar = ({ isOpen }) => {
   return (
     <div
       className={`
-    bg-zinc-900 rounded-lg p-4 text-white
-    fixed md:static top-0 left-0 md:h-auto z-50
-    transition-transform duration-300
-    
-    w-[70%] sm:w-[50%] md:w-[25%] lg:w-[20%]
+        bg-[#121212] text-white flex flex-col rounded-lg
+        fixed md:static top-0 left-0 h-screen md:h-auto z-50
+        transition-transform duration-300
+        
+        w-[80%] sm:w-[60%] md:w-[26%] lg:w-[20%]
 
-    ${isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"}
-  `}
+        ${isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"}
+      `}
     >
-      <h2 className="font-bold text-lg mb-4">Your Library</h2>
+      <div className="px-4 py-4 flex items-center justify-between">
+        <h2 className="text-sm font-semibold text-gray-300">
+          Your Library
+        </h2>
 
-      <ul className="flex flex-col gap-3 text-sm">
-        <li className="hover:text-white cursor-pointer">Home</li>
-        <li className="hover:text-white cursor-pointer">Search</li>
-        <li className="hover:text-white cursor-pointer">Your Library</li>
-      </ul>
+        <button className="bg-[#1f1f1f] h-8 w-8 cursor-pointer rounded-full hover:bg-[#2a2a2a] transition">
+          +
+        </button>
+      </div>
+      <div className="flex-1 overflow-y-auto no-scrollbar px-2 space-y-3">
+
+        <div className="bg-[#1f1f1f] p-4 rounded-lg space-y-3 hover:bg-[#2a2a2a] transition">
+          <h3 className="text-sm font-semibold">
+            Create your first playlist
+          </h3>
+          <p className="text-xs text-gray-400">
+            It's easy, we'll help you
+          </p>
+          <button className="cursor-pointer bg-white text-black px-4 py-1.5 rounded-full text-sm font-semibold hover:scale-105 transition">
+            Create playlist
+          </button>
+        </div>
+
+        <div className="bg-[#1f1f1f] p-4 rounded-lg space-y-3 hover:bg-[#2a2a2a] transition">
+          <h3 className="text-sm font-semibold">
+            Let's find some podcasts to follow
+          </h3>
+          <p className="text-xs text-gray-400">
+            We'll keep you updated on new episodes
+          </p>
+          <button className="cursor-pointer bg-white text-black px-4 py-1.5 rounded-full text-sm font-semibold hover:scale-105 transition">
+            Browse podcasts
+          </button>
+        </div>
+
+      </div>
+
+      <div className="px-4 py-4 text-[11px] text-gray-400 space-y-3">
+        
+        <div className="flex flex-wrap gap-3">
+          <span className="hover:underline cursor-pointer">Legal</span>
+          <span className="hover:underline cursor-pointer">Safety & Privacy</span>
+          <span className="hover:underline cursor-pointer">Privacy Policy</span>
+          <span className="hover:underline cursor-pointer">Cookies</span>
+          <span className="hover:underline cursor-pointer">Accessibility</span>
+        </div>
+
+        <button className="border border-gray-500 px-3 flex items-center gap-2 cursor-pointer py-1 rounded-full text-white text-xs hover:border-white transition">
+          <span className="ri-global-line text-lg"></span> English
+        </button>
+      </div>
     </div>
   );
 };
