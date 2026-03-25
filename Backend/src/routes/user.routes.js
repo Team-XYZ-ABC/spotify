cf264337fd416e099fbcacf7ae5f3ce46fa44b00
Md Alkama <mdalkamadad@gmail.com>
2026-03-25 06:29:47 +0530

    fix: enhance user profile handling with detailed responses and validation

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -5,7 +5,7 @@ import isAuthenticated from "../middlewares/auth.middleware.js";
 const userRouter = Router()
 
 userRouter.get('/me', isAuthenticated, getProfile)
-userRouter.patch('/me', updateProfile)
+userRouter.patch('/me', isAuthenticated , updateProfile)
 userRouter.get('/:id', getOtherUserProfile)
 userRouter.get('/me/history', getHistory)
 userRouter.get('/me/recently-played', getRecentlyPlayed)
