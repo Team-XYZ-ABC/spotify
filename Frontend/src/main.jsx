c2b3cad6766fdba0e0f3e4c079079ddfe3c2f506
Md Alkama <mdalkamadad@gmail.com>
2026-03-29 18:58:07 +0530

    feat: wrap application in Redux Provider for state management

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Router } from "./routes/Main.routes.jsx";
 import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
+import store from "./redux/app.redux.jsx";
 
 createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
     <RouterProvider router={Router}>
         <App />
     </RouterProvider>,
+    </Provider>
 );
